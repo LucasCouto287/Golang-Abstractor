aff08edac5ac9aa374277be7828af7f232321ead
Nrup R Patel <nruppatel133@gmail.com>
2019-11-04 00:27:39 -0500

    Update links from master

--- proton.go
+++ proton.go
@@ -1,1 +1,97 @@
 package main
+
+
+import "fmt"
+import "" // get links from master
+import "os"
+import ""// get links from master
+import ""// get links from master
+import "log"
+
+type ObjectStore interface {
+	Auth() string
+	ListBuckets() string
+	CreateBucket(bucketName string) string
+	DeleteBucket(bucketName string) string
+	Put(src string, dst string) string
+	Get() string
+	Del() string
+}
+
+type ObjectStoreService struct {
+	ObjectStore string
+	AuthInfo    json.RawMessage
+}
+
+func main() {
+	objstore := loadConfig()
+	objstore.Auth()
+	app := cli.NewApp()
+	app.Name = "proton"
+	app.Usage = "Object Storage Abstractor"
+	app.Action = func(c *cli.Context) {
+		println("Hello friend!")
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:      "list",
+			ShortName: "l",
+			Usage:     "list all avaiable buckets/container",
+			Action: func(c *cli.Context) {
+				objstore.ListBuckets()
+			},
+		},
+		{
+			Name:      "delete",
+			ShortName: "d",
+			Usage:     "delete a bucket/container",
+			Action: func(c *cli.Context) {
+				objstore.DeleteBucket(c.Args().First())
+			},
+		},
+		{
+			Name:      "create",
+			ShortName: "c",
+			Usage:     "create a bucket/container",
+			Action: func(c *cli.Context) {
+				objstore.CreateBucket(c.Args().First())
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
+
+func loadConfig() ObjectStore {
+	var oss ObjectStoreService
+	var filename = ""
+	if os.Getenv("PROTON_CONFIG") != "" {
+		filename = os.Getenv("PROTON_CONFIG")
+	} else if _, err := os.Stat("/etc/proton/proton.cfg"); !os.IsNotExist(err) {
+		filename = "/etc/proton/proton.cfg"
+	} else if _, err := os.Stat("~/.proton/proton.cfg"); !os.IsNotExist(err) {
+		filename = "~/.proton/proton.cfg"
+	} else if _, err := os.Stat("proton.cfg"); !os.IsNotExist(err) {
+		filename = "proton.cfg"
+	} else {
+		log.Fatal("Config file not found")
+	}
+	file, _ := ioutil.ReadFile(filename)
+	err := json.Unmarshal(file, &oss)
+	var objstore ObjectStore
+	switch oss.ObjectStore {
+	case "s3":
+		objstore = new(AWS_S3)
+	case "swift":
+		objstore = new(OS_Swift)
+	case "local":
+		objstore = new(Local)
+	}
+
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	json.Unmarshal(oss.AuthInfo, objstore)
+	return objstore
+}
